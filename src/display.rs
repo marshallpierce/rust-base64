f4747d43161f10a03a09da9dd5cdf7494fe19a61
Marshall Pierce <marshall@mpierce.org>
2017-04-30 14:45:33 -0700

    Add internal ChunkedEncoder.
    This uses a stack-resident fixed-length buffer to encode, heap-
    free sorta-streaming-style.
    
    Also, added a Display implementation using ChunkedEncoder to allow
    base64'ing in format strings. It's about 20% slower than the usual
    straightforward encoding.

---
+++ src/display.rs
@@ -0,0 +1,71 @@
+use super::{STANDARD, Config};
+use super::chunked_encoder::{ChunkedEncoder, ChunkedEncoderError};
+use std::fmt::{Display, Formatter};
+use std::fmt;
+
+/// A convenience wrapper for base64'ing bytes into a format string without heap allocation.
+pub struct Base64Display<'a> {
+    bytes: &'a [u8],
+    chunked_encoder: ChunkedEncoder
+}
+
+impl<'a> Base64Display<'a> {
+    /// Create a Base64Display with default base64 configuration: no line wrapping, with padding.
+    pub fn new(bytes: &[u8]) -> Base64Display {
+        Self::new_with_config(bytes, STANDARD).expect("STANDARD is always ok")
+    }
+
+    fn new_with_config(bytes: &[u8], config: Config) -> Result<Base64Display, ChunkedEncoderError> {
+        ChunkedEncoder::new(config).map( |c| Base64Display {
+            bytes: bytes,
+            chunked_encoder: c
+        })
+    }
+}
+
+impl<'a> Display for Base64Display<'a> {
+    fn fmt(&self, formatter: &mut Formatter) -> Result<(), fmt::Error> {
+        let mut sink = FormatterSink { f: formatter };
+        self.chunked_encoder.encode(self.bytes, &mut sink)
+    }
+}
+
+struct FormatterSink<'a, 'b: 'a> {
+    f: &'a mut Formatter<'b>
+}
+
+impl<'a, 'b: 'a> super::chunked_encoder::Sink for FormatterSink<'a, 'b> {
+    type Error = fmt::Error;
+
+    fn write_str(&mut self, s: &str) -> Result<(), Self::Error> {
+        self.f.write_str(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::super::*;
+    use super::super::chunked_encoder::tests::{SinkTestHelper, chunked_encode_matches_normal_encode_random};
+
+    #[test]
+    fn basic_display() {
+        assert_eq!("~$Zm9vYmFy#*", format!("~${}#*", Base64Display::new("foobar".as_bytes())));
+        assert_eq!("~$Zm9vYmFyZg==#*", format!("~${}#*", Base64Display::new("foobarf".as_bytes())));
+    }
+
+    #[test]
+    fn display_encode_matches_normal_encode() {
+        let helper = DisplaySinkTestHelper;
+        chunked_encode_matches_normal_encode_random(&helper);
+    }
+
+    struct DisplaySinkTestHelper;
+
+    impl SinkTestHelper for DisplaySinkTestHelper {
+        fn encode_to_string(&self, config: Config, bytes: &[u8]) -> String {
+            format!("{}", Base64Display::new_with_config(bytes, config).unwrap())
+        }
+    }
+
+}
