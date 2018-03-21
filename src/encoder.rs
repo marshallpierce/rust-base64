2995d38cb64aec440fa4c77b531df26d30d0f174
Marshall Pierce <marshall@mpierce.org>
2018-03-21 09:16:57 -0500

    Add a randomized test. Fix bug that choked on large input slices. Reformat and use longer var names.

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use encode::encode_to_slice;
 use {encode_config_slice, Config};
 use std::io::{Result, Write};
 
@@ -38,59 +39,69 @@ impl<'a> Base64Encoder<'a> {
 
 impl<'a> Write for Base64Encoder<'a> {
     fn write(&mut self, input: &[u8]) -> Result<usize> {
-        let mut p = input;
+        // TODO handle line breaks
+        let mut input = input;
         let mut input_read_cnt = 0;
 
         // process leftover stuff from last write
         if self.extra_len > 0 {
             let mut i = 0;
-            while i < p.len() && self.extra_len < 3 {
-                self.extra[self.extra_len] = p[i];
+            while i < input.len() && self.extra_len < 3 {
+                self.extra[self.extra_len] = input[i];
                 self.extra_len += 1;
                 i += 1;
             }
             input_read_cnt += i;
-            p = &p[i..];
+            input = &input[i..];
+
             if self.extra_len < 3 {
                 // not enough to actually encode, yet.
                 return Ok(input_read_cnt);
             }
-            let sz = encode_config_slice(&self.extra[..3], self.config, &mut self.output[..]);
+
+            let encoded_size = encode_to_slice(
+                &self.extra[..3],
+                &mut self.output[..],
+                self.config.char_set.encode_table(),
+            );
             self.extra_len = 0;
-            let _ = self.w.write(&self.output[..sz])?;
+            let _ = self.w.write(&self.output[..encoded_size])?;
         }
 
         // process chunks, 768 bytes -> 1024 (encoded) bytes, at a time
-        let mut output_cnt = 0;
-        while p.len() >= 3 {
-            let mut nn = (self.output.len() / 4) * 3;
-            if nn > p.len() {
+        // TODO try using `chunks()`
+        while input.len() >= 3 {
+            let mut bytes_to_encode = (self.output.len() / 4) * 3;
+            if bytes_to_encode > input.len() {
                 // force nn to be a multiple of three o we can encode cleanly
-                nn = p.len();
-                nn -= nn % 3;
+                bytes_to_encode = input.len();
+                bytes_to_encode -= bytes_to_encode % 3;
             }
-            let sz = encode_config_slice(&p[..nn], self.config, &mut self.output[..]);
-            input_read_cnt += nn;
-            output_cnt += sz;
-            p = &p[nn..];
+            let encoded_size = encode_to_slice(
+                &input[..bytes_to_encode],
+                &mut self.output[..],
+                self.config.char_set.encode_table(),
+            );
+            input_read_cnt += bytes_to_encode;
+            input = &input[bytes_to_encode..];
+            let _ = self.w.write(&self.output[..encoded_size])?;
         }
 
         // stash leftover bytes
         let mut i = 0;
-        while i < p.len() {
-            self.extra[i] = p[i];
+        while i < input.len() {
+            self.extra[i] = input[i];
             i += 1;
         }
-        input_read_cnt += p.len();
-        self.extra_len = p.len();
-
-        let _ = self.w.write(&self.output[..output_cnt])?;
+        input_read_cnt += input.len();
+        self.extra_len = input.len();
 
         return Ok(input_read_cnt);
     }
 
     fn flush(&mut self) -> Result<()> {
         if self.extra_len > 0 {
+            // TODO should not write padding on each flush() -- should be able to flush() > 1x
             let sz = encode_config_slice(
                 &self.extra[..self.extra_len],
                 self.config,
@@ -105,9 +116,17 @@ impl<'a> Write for Base64Encoder<'a> {
 
 #[cfg(test)]
 mod tests {
-    use encoder::Base64Encoder;
-    use {encode_config, URL_SAFE};
+    extern crate rand;
+
+    use super::Base64Encoder;
+    use {encode_config, encode_config_buf, URL_SAFE};
+    use tests::random_config;
+
     use std::io::{Cursor, Write};
+    use std::str;
+
+    use self::rand::Rng;
+    use self::rand::distributions::range;
 
     #[test]
     fn encode_three_bytes() {
@@ -219,5 +238,55 @@ mod tests {
             &c.get_ref()[..],
             encode_config("abcdefg", URL_SAFE).as_bytes()
         );
+    }
+
+    #[test]
+    fn encode_random_config_matches_normal_encode() {
+        let mut rng = rand::thread_rng();
+        let mut orig_data = Vec::<u8>::new();
+        let mut stream_encoded = Vec::<u8>::new();
+        let mut normal_encoded = String::new();
+        let line_len_range = range::Range::new(1, 2000);
+
+        for _ in 0..1_000 {
+            orig_data.clear();
+            stream_encoded.clear();
+            normal_encoded.clear();
+
+            // TODO for now, ignore configs with line wraps
+            let mut config = random_config(&mut rng, &line_len_range);
+            while let ::LineWrap::Wrap(_, _) = config.line_wrap {
+                config = random_config(&mut rng, &line_len_range)
+            }
+
+            let orig_len: usize = rng.gen_range(100, 10_000);
+            for _ in 0..orig_len {
+                orig_data.push(rng.gen());
+            }
+
+            // encode the normal way
+            encode_config_buf(&orig_data, config, &mut normal_encoded);
+
+            // encode via the stream encoder
+            {
+                let mut stream_encoder = Base64Encoder::new(&mut stream_encoded, config);
+                let mut bytes_consumed = 0;
+                while bytes_consumed < orig_len {
+                    let input_len: usize = rng.gen_range(0, orig_len - bytes_consumed + 1);
+
+                    // write a little bit of the data
+                    bytes_consumed += stream_encoder
+                        .write(&orig_data[bytes_consumed..bytes_consumed + input_len])
+                        .unwrap();
+                }
+
+                // TODO final write should be done by drop()
+                stream_encoder.flush().unwrap();
+
+                assert_eq!(orig_len, bytes_consumed);
+            }
+
+            assert_eq!(normal_encoded, str::from_utf8(&stream_encoded).unwrap());
+        }
     }
 }
