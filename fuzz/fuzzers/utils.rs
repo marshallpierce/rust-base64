b99cbdf0b6050e364a929838e3a43db3344d15f1
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2019-01-19 15:39:58 +0300

    use sha2 crate

--- fuzz/fuzzers/utils.rs
+++ fuzz/fuzzers/utils.rs
@@ -1,21 +1,21 @@
 extern crate base64;
 extern crate rand;
 extern crate rand_pcg;
-extern crate ring;
+extern crate sha2;
 
 use self::base64::*;
 use self::rand::{Rng, SeedableRng};
 use self::rand_pcg::Pcg32;
-use self::ring::digest;
+use self::sha2::{Sha256, Digest};
 
 pub fn random_config(data: &[u8]) -> Config {
     // use sha256 of data as rng seed so it's repeatable
-    let sha = digest::digest(&digest::SHA256, data);
+    let sha = Sha256::digest(data);
 
     let mut seed: [u8; 16] = [0; 16];
-    seed.copy_from_slice(&sha.as_ref()[0..16]);
+    seed.copy_from_slice(&sha[..16]);
 
-    let mut rng = Pcg32::from_seed(seed);
+    let mut rng = Pcg32::from_seed(&sha);
 
     let charset = if rng.gen() {
         CharacterSet::UrlSafe
