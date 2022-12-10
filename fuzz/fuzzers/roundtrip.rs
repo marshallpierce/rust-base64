f58f44168920b008d14de437b39a87d0f520f1d6
Marshall Pierce <575695+marshallpierce@users.noreply.github.com>
2022-12-10 11:49:31 -0700

    Also updated fuzzers

--- fuzz/fuzzers/roundtrip.rs
+++ fuzz/fuzzers/roundtrip.rs
@@ -5,7 +5,7 @@ extern crate base64;
 use base64::engine::DEFAULT_ENGINE;
 
 fuzz_target!(|data: &[u8]| {
-    let encoded = base64::encode_engine(&data, &DEFAULT_ENGINE);
+    let encoded = base64::encode_engine(data, &DEFAULT_ENGINE);
     let decoded = base64::decode_engine(&encoded, &DEFAULT_ENGINE).unwrap();
     assert_eq!(data, decoded.as_slice());
 });
